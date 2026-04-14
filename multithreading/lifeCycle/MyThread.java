ca557a17947c738fc5b5732a7152276494e52f0a
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2026-04-14 04:59:17 +0000

    Life cycle sorted Methods in thread : t.start() t.getState() t.join() - to terminate Thread.curretnThread().name()

---
+++ multithreading/lifeCycle/MyThread.java
@@ -0,0 +1,34 @@
+package lifeCycle;
+
+ public class MyThread  extends Thread{
+   
+
+   @Override
+   public void run() {
+    try {
+        System.out.println("Running oye");
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+ System.out.println(e);
+    }
+
+   }
+
+   public static void main(String [] args) throws InterruptedException{
+
+
+    MyThread t1 = new MyThread();
+    System.out.println(t1.getState());
+    t1.start();
+    System.out.println(t1.getState());
+
+   
+   Thread.sleep(100);
+   System.out.println(t1.getState());
+   t1.join();
+   System.out.println(t1.getState());
+    
+   }
+
+   
+}
