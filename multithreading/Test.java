b4fdf5ed11d921da0c495db19e8327f08317d9d2
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2026-04-14 04:14:13 +0000

    Multi threading using Extend Thread class, 1  Make class extending Thread 2 Override run method 3 create object of this class 4 call start method

--- multithreading/Test.java
+++ multithreading/Test.java
@@ -2,6 +2,12 @@ public class Test {
 
     public static void main(String []args){
         System.out.println("Chal gyaa");
+
+        World world = new World();
+        world.start();
+      
+      for( ; ;){
         System.out.println(Thread.currentThread().getName());
+      }
     }
 }
