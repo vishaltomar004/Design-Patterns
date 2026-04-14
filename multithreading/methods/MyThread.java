ddbcc8b74a99a009039908b4f8e1ba3a62c5e76d
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2026-04-14 05:48:33 +0000

    Yield method and interrupt method

--- multithreading/methods/MyThread.java
+++ multithreading/methods/MyThread.java
@@ -1,59 +1,41 @@
 package methods;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
 
-    public MyThread(String name){
-        super(name);
-    }
-
-    // @Override
-    // public void run(){
-
-    //     try {
-
-    //         System.out.println("Thread is running aa");
-        
-    //     }catch (Exception e) {
-    //         // TODO: handle exception
-    //     }
-
-// Set priority
     @Override
-    public void run(){
+    public void run() {
 
-        for(int i =0 ; i< 5; i++){
-            for(int j =0 ; j<1000000000 ; j++){
+        // Code for interrupt method
+        // try {
 
-            }
-            System.out.println(Thread.currentThread().getName() + " - Priority : " + Thread.currentThread().getPriority() + " - count  " + i );
+        // Thread.sleep(1000);
+        // System.out.println("Running , this will not print after interruption method
+        // beingcalld");
+        // } catch (InterruptedException e) {
+        // System.out.println("Thread interrupted : " + e);
+        // }
 
-            try{
-                Thread.sleep(1000);
-            }
-            catch(Exception e){
-
-            }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " is running ");
+            Thread.yield();  // Hint de rahe hai ki dusre threads ko bhi chance do 
         }
+
     }
-    public static void main(String[] args)  throws InterruptedException{
+
+    public static void main(String[] args) throws InterruptedException {
+
+    //     MyThread l = new MyThread();
+    //     l.start();
+    //     l.interrupt() ;  // means what ever l  is doing stop it
+
+    MyThread t1 = new MyThread();
+    MyThread t2 = new MyThread();
+
+t1.start();
+t2.start();
 
 
-
-         MyThread l = new MyThread("Low Priority Thread");
-         MyThread m = new MyThread("Medium Priority Thread");
-         MyThread n = new MyThread("High Priority Thread");
-
-         l.setPriority(Thread.MIN_PRIORITY);
-         m.setPriority(Thread.NORM_PRIORITY);
-         n.setPriority(Thread.MAX_PRIORITY);
-
-         l.start();
-         m.start();
-         n.start();
-
-        
     }
 }
 
-
-// start , run , sleep  , join , set priority
+// start , run , sleep , join , set priority , yeild
