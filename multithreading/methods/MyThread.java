3cfa36780ce18ef041dc19c8ba342cd0f9c454ad
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2026-04-14 05:55:13 +0000

    Daemon thread is cool thinng , Suppore we marked a thread as Daemon thread then if all thread are terminated then it will also terminate , like it it is used in an infinite loop then will will not run foreever  ,it will see ohh other threads are terminated now I am also will be stopped

--- multithreading/methods/MyThread.java
+++ multithreading/methods/MyThread.java
@@ -15,27 +15,38 @@ public class MyThread extends Thread {
         // System.out.println("Thread interrupted : " + e);
         // }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " is running ");
-            Thread.yield();  // Hint de rahe hai ki dusre threads ko bhi chance do 
+        // -------------- for yield -----------
+        // for (int i = 0; i < 5; i++) {
+        // System.out.println(Thread.currentThread().getName() + " is running ");
+        // Thread.yield(); // Hint de rahe hai ki dusre threads ko bhi chance do
+        // }
+
+        // ----------------- for Daemon ----------------  works in backend , JVM  does not waits for them 
+//         //Daemon thread is cool thinng , 
+// Suppore we marked a thread as Daemon thread then if all thread are terminated then it will also terminate , like it it is used in an infinite loop then will will not run foreever  ,it will see ohh other threads are terminated now I am also will be stopped
+        while (true) {
+            System.out.println("Hellow world");
         }
 
     }
 
     public static void main(String[] args) throws InterruptedException {
 
-    //     MyThread l = new MyThread();
-    //     l.start();
-    //     l.interrupt() ;  // means what ever l  is doing stop it
+        // MyThread l = new MyThread();
+        // l.start();
+        // l.interrupt() ; // means what ever l is doing stop it
 
-    MyThread t1 = new MyThread();
-    MyThread t2 = new MyThread();
+        // MyThread t1 = new MyThread();
+        // MyThread t2 = new MyThread();
 
-t1.start();
-t2.start();
+        // t1.start();
+        // t2.start();
 
+ MyThread t1 = new MyThread();
+t1.setDaemon(true);
+ t1.start();
 
     }
 }
 
-// start , run , sleep , join , set priority , yeild
+// start , run , sleep , join , set priority , yeild , setDaemon
